d55c1d06304fdd8f79c14ccd20f7cbfc0f138031
helinveysanogluu <helinveysanogluu@gmail.com>
2025-11-09 12:08:08 +0300

    The add method has been fixed and the delete method has been added.

--- src/main/java/org/example/BookList.java
+++ src/main/java/org/example/BookList.java
@@ -9,18 +9,41 @@ public class BookList {
         last = null;
     }
 
-    void addBook(BookInfo newBook) {
-
+    void addBook(String bookName, String authorName, String bookStatus, int barcodeNumber, int publicationYear, int numberOfPages) {
+        BookInfo newBook = new BookInfo(bookName, authorName, bookStatus, barcodeNumber, publicationYear, numberOfPages);
         if (first == null) {
             first = newBook;
             last = newBook;
-            return;
         } else {
-            newBook.forward = first;
-            first = newBook;
+            last.forward = newBook;
+            last = newBook;
         }
-
     }
+
+    void removeBook(int barcodeNumber) {
+        BookInfo temp = first;
+        BookInfo before = null;
+
+        while (temp != null) {
+            if (temp.barcodeNumber == barcodeNumber) {
+                if (temp == first) {
+                    first = temp.forward;
+                }
+                else if (temp == last) {
+                    last = before;
+                    before.forward = null;
+                }
+                else {
+                    before.forward = temp.forward;
+                }
+                System.out.println("Kitap silindi: " + temp.bookName);
+                return;
+            }
+            before = temp;
+            temp = temp.forward;
+        }
+        System.out.println("Barkod numarası bulunamadı.");
+        }
 
     void printList() {
         BookInfo temp = first;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        BookInfo book = new BookInfo("Tutunamayanlar", "Oğuz Atay", "In Library", 123456789, 1972, 724);
         BookList bl = new BookList();
 
-        bl.addBook(book);
+        bl.addBook("Tutunamayanlar", "Oğuz Atay", "In Library", 123456789, 1972, 724);
+        bl.printList();
+        System.out.println("--------------------------------");
+        bl.addBook("Hayvan Çiftliği", "George Orwell", "In Library", 13579, 1980, 386);
+        bl.printList();
+
+        System.out.println("--------------------------------");
+        bl.removeBook(123456789);
         bl.printList();
     }
 }
